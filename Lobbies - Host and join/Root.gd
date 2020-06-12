9bd09e5f068c8150678ad6bdc250cf87ed892582
MickeMakaron <mikael.hernvall@gmail.com>
2020-06-12 09:46:05 +0200

    Simplify project

--- Lobbies - Host and join/Root.gd
+++ Lobbies - Host and join/Root.gd
@@ -1,17 +1,17 @@
 extends Control
 
 
-func _on_Gotm_lobby_changed():
-	if not Gotm.lobby:
-		$Lobbies.show()
-		$Game.hide()
-
+onready var _fetch = GotmLobbyFetch.new()
 
 func _ready():
 	Gotm.connect("lobby_changed", self, "_on_Gotm_lobby_changed")
 	$Game.hide()
+	$Lobbies/List/LobbyEntry.hide()
+	_on_Refresh_clicked(null)
+	
 
-func _on_Lobbies_join_pressed(lobby: GotmLobby):
+
+func _on_LobbyEntry_selected(lobby):
 	$Lobbies.hide()
 	$Game.show()
 	
@@ -23,11 +23,38 @@ func _on_Lobbies_join_pressed(lobby: GotmLobby):
 		$Game.hide()
 
 
-func _on_Lobbies_host_pressed(name: String):
+func _on_Host_clicked(instance):
 	$Lobbies.hide()
 	$Game.show()
 	
 	Gotm.host_lobby(false)
 	Gotm.lobby.hidden = false
-	Gotm.lobby.name = name
+	Gotm.lobby.name = $Lobbies/Host/Name.get_text()
 	$Game.host()
+
+
+func _on_Refresh_clicked(instance):
+	$Lobbies/List/Spinner.show()
+	for child in $Lobbies/List/Entries.get_children():
+		child.queue_free()
+	
+	var lobbies = yield(_fetch.first(5), "completed")
+	
+	for i in range(lobbies.size()):
+		var lobby = lobbies[i]
+		var node = $Lobbies/List/LobbyEntry.duplicate()
+		node.show()
+		$Lobbies/List/Entries.add_child(node)
+		node.set_lobby(lobby)
+		node.rect_position.y += i * 80
+	
+	$Lobbies/List/Spinner.hide()
+
+
+func _on_Gotm_lobby_changed():
+	if not Gotm.lobby:
+		$Lobbies.show()
+		$Game.hide()
+
+
+

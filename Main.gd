39383d0c60bfdeb7b6798e385a1f1ca9b2eeb563
Mikael Hernvall <mikael.hernvall@gmail.com>
2022-09-06 16:06:26 +0200

    Add options gui

--- Main.gd
+++ Main.gd
@@ -25,8 +25,38 @@ const OPTIONS = {
 	}
 }
 
-func _ready():
-	
 
+func _ready():
+	for option_key in OPTIONS:
+		if OS.has_feature("option_" + option_key):
+			get_tree().change_scene(OPTIONS[option_key].path)
+			return
+			
+	
+	var y = 0
+	for option_key in OPTIONS:
+		var button := _create_option_button(OPTIONS[option_key])
+		button.rect_position.y += y
+		y += button.rect_size.y * 1.1	
+		button.name = option_key
+		add_child(button)
 	return
 
+func _create_option_button(option) -> Control:
+	var button_template := $Button
+	var button: Control = button_template.duplicate()
+	button.visible = true
+	button.text = option.name
+	var description: Label = button.get_node("Description")
+	var label: Label = button.get_node("Label")
+	label.valign = Label.VALIGN_TOP
+	label.margin_top = label.rect_size.y * 0.5
+	description.text = option.description
+
+	return button
+	
+
+
+
+func _on_Button_clicked(button: Control):
+	get_tree().change_scene(OPTIONS[button.name].path)
